a7f2724bbbba207ea9ce4e0a620286377bfe1d36
SneakBug8 <pavlikelf@gmail.com>
2016-10-19 13:20:44 +0400

    Update ch_color.py

--- ch_color.py
+++ ch_color.py
@@ -1,21 +1,22 @@
-long=3
-file="001005101"
+long=int(input())
+file=input()
 res=""
 aa=1
 i=1
 t1=-3
-change="001"
-changeto="100"
+cpp=3
+change=input()
+changeto=input()
 while aa==1:
-    t1=t1+3
-    t2=file[t1:t1+3]
+    t1=t1+cpp
+    t2=file[t1:t1+cpp]
     if (t2==change):
         t3=changeto
     else:
         t3=t2
     res=res+t3 
-    print (res)
     i=i+1
     if i>long:
         aa=0;
+        print(res)
     
