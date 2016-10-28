b4b737ea9c67c818d531ad76c90f6b58e21f8e42
SneakBug8 <pavlikelf@gmail.com>
2016-10-28 16:57:44 +0400

    Update Prost-Slogn.py

--- Prost-Slogn.py
+++ Prost-Slogn.py
@@ -1,8 +1,13 @@
 n=int(input())
 import math; 
+
 res="Simple"
-for i in range(2,int(math.sqrt(n+1))):
-    o=n%i
-    if(o==0):
-        res="Not simple"
-print (res)
+    if n>20:
+       op=int(math.sqrt(n+1))
+    else:
+        op=n
+    for i in range(2,op):
+        o=n%i
+        if(o==0):
+            res="Not simple"
+    print (res)
